0980058d5d2d7c844f4ae68e8959a5d0fd41f224
Pierce Collazo <pierce.collazo@codeimmersive.com>
2019-03-27 12:50:18 -0400

    filled in data.js with an array of objects

--- data.js
+++ data.js
@@ -1,3 +1,29 @@
-const data = [
-    
-]
+const armata = {
+    name: 'T-14 Armata',
+    origin: 'Russian Federation',
+    weight: 48,
+    manufacturer: 'Uralvagonzavod',
+    speed: 50,
+    caliber: 125
+}
+
+const abrams = {
+    name: 'M1A2 Abrams',
+    origin: 'United States of America',
+    weight: 70,
+    manufacturer: 'General Dynamics',
+    speed: 42,
+    caliber: 120
+}
+
+const challenger = {
+    name: 'FV4034 Challenger 2',
+    origin: 'United Kingdom',
+    weight: 75,
+    manufacturer: 'BAE',
+    speed: 37,
+    caliber: 120
+}
+
+
+const data = [armata,abrams,challenger]
